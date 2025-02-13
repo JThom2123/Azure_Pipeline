1ef13c4f988d1c3f4b4a3cda08455a437ddf6687
Georgia Gelasco <ggelasc@clemson.edu>
2025-02-13 19:53:44 +0000

    change admin privleges

--- databaseUserAccess.sql
+++ databaseUserAccess.sql
@@ -1,6 +1,6 @@
 -- give the "admin" user full privleges
 CREATE USER 'admin'@'%' IDENTIFIED BY 'adminPassword';
-GRANT ALL PRIVLEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
+GRANT SELECT, INSERT, UPDATE, DELETE ON mydatabase.* TO 'admin'@'%';
 
 -- give the "database manager" user full privleges
 CREATE USER 'database_manager'@'%' IDENTIFIED BY 'managerPassword';
