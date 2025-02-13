e914a683f5f5d04a78312cf500d0a849f140e7af
Tristan-Eye9 <tecpetit@gmail.com>
2025-02-13 18:09:11 -0500

    Adjusted RDS docs

--- MySQL Queries/About_Table_Queries.sql
+++ MySQL Queries/About_Table_Queries.sql
@@ -1,4 +1,13 @@
--- This is a basic insert query
+-- initialize the table --
+
+create TABLE about_page (
+    section_name VARCHAR(255),
+    content TEXT,
+    last_updated TIMESTAMP default current_timestamp on update current_timestamp
+);
+
+-- Basic table filling queries to populate our about page table --
+
 Insert into about_page (section_name, content)
 Values ('Version Number', '0.0.1');
 
@@ -14,5 +23,12 @@ values ('product_name', 'Not Yet Decided');
 insert into about_page (section_name, content)
 values ('product_description', 'Hi there. Our Product Tracks you. Everywhere. No matter where you go. You cannot escape. No matter what you do. You are now a trucker for life.');
 
--- display the table
+-- modifying rows --
+
+update about_page
+set content = 'Hi there. Our Product Tracks you. Everywhere. No matter where you go. You cannot escape. No matter what you do. You are now a trucker for life. A mother trucker.'
+where section_name = 'product_description'; 
+
+-- Show the table --
+
 SELECT * FROM Team22DB.about_page;
