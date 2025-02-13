56957f9a7cde241a82b9af862d239429e24e6ce3
Georgia Gelasco <ggelasc@clemson.edu>
2025-02-13 20:12:46 +0000

    next steps

--- databaseUserAccess.sql
+++ databaseUserAccess.sql
@@ -9,3 +9,6 @@ GRANT ALL PRIVLEGES ON *.* TO TO 'database_manager'@'%';
 
 -- apply the changes
 FLUSH PRIVILEGES;
+
+-- mysql -h cpsc4910-team22-db.cobd8enwsupz.us-east-1.rds.amazonaws.com -P 3306 -u admin -p ***password***
+-- then once the sql is entered add this
