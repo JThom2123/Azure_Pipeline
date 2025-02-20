1491228b3146683652c584dbf81d65c441645474
Georgia Gelasco <ggelasc@clemson.edu>
2025-02-20 20:30:39 +0000

    password added

--- databaseUserAccess.sql
+++ databaseUserAccess.sql
@@ -10,5 +10,5 @@ GRANT ALL PRIVLEGES ON *.* TO TO 'database_manager'@'%';
 -- apply the changes
 FLUSH PRIVILEGES;
 
--- mysql -h cpsc4910-team22-db2.cobd8enwsupz.us-east-1.rds.amazonaws.com -P 3306 -u admin -p ***password***
+-- mysql -h cpsc4910-team22-db2.cobd8enwsupz.us-east-1.rds.amazonaws.com -P 3306 -u admin -p ThereWasATeamHere!
 -- then once the sql is entered add this
