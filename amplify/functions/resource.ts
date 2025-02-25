0c6775b4fa12f18fd3535d55b5c0920ed2ecf038
jkthomp <jkthomp@g.clemson.edu>
2025-02-25 14:46:45 -0500

    added the lambda code

--- amplify/functions/resource.ts
+++ amplify/functions/resource.ts
@@ -2,7 +2,7 @@ import { defineFunction } from "@aws-amplify/backend";
 
 export const funct = defineFunction({
     // optionally specify a name for the Function (defaults to directory name)
-    name: 'say-hello',
+    name: 'team22 - lambda',
     // optionally specify a path to your handler (defaults to "./handler.ts")
     entry: './handler.ts'
   });
