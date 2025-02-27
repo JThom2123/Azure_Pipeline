71d3bfe5d2e594bbddea8be8b18153d698dbe5b7
Trisha Andres <trishaa@clemson.edu>
2025-02-27 15:59:22 +0000

    Updated layout.tsx - add auth for debugging

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ Amplify.configure({
     region: "us-east-1", 
     userPoolId: "us-east-1_SBC9pg6ag", 
     userPoolWebClientId: "107720oslv197r2qvlb4h1ptcs", 
+    oauth: {},
     mandatorySignIn: true, 
   },
 });
