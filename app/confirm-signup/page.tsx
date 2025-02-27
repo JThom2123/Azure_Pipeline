442c60f761a89e7139123271588948c818727374
Trisha Andres <trishaa@clemson.edu>
2025-02-27 07:20:51 +0000

    Updated page.tsx

--- app/confirm-signup/page.tsx
+++ app/confirm-signup/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { confirmSignUp } from "../../lib/auth";
+import { userConfirmSignUp } from "../../lib/auth";
 
 export default function ConfirmSignUpPage() {
     const router = useRouter();
