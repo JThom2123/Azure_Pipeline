49872bef50df6531fa74c689df07dc1c7a3d216d
Tristan-Eye9 <tecpetit@gmail.com>
2025-03-03 12:36:09 -0500

    updated lambda

--- lambda/index.mjs
+++ lambda/index.mjs
@@ -6,24 +6,31 @@ const API_STAGE = '/dev1';
 
 export const handler = async (event) => {
     // Extract method and path from requestContext
+
     const httpMethod = event.requestContext.http.method;
     const path = event.requestContext.http.path;
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    const schema = 'Team22DB';
 
     // Log the method and path for debugging
     console.log(`Received request with method: ${httpMethod}, path: ${path}`);
 
     try {
         const connection = await mysql.createConnection({
-            host: 'cpsc4910-team22-db2.cobd8enwsupz.us-east-1.rds.amazonaws.com',
+            host: 'cpsc4911.cobd8enwsupz.us-east-1.rds.amazonaws.com',
             user: 'admin',
-            password: 'ThereWasATeamHere!',
+            password: '4911Admin2025',
             database: 'Team22DB'
         });
 
         let response;
 
         if (httpMethod === 'GET' && path === `${API_STAGE}/about`) {
-            const [rows] = await connection.execute('SELECT * FROM about_page');
+            
+            //Set schema to our own DB schema
+            await connection.query(`USE ${schema}`);
+
+            const [rows] = await connection.execute(`SELECT * FROM about_page`);
             console.log('Query Result:', rows);
 
             response = {
