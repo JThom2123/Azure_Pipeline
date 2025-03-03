f0e25f78b140d2a75e8c2f6b886dcfeb6601f989
Georgia Gelasco <ggelasc@clemson.edu>
2025-03-03 16:02:01 +0000

    remove updated buttons

--- app/aboutpage/page.tsx
+++ app/aboutpage/page.tsx
@@ -107,19 +107,14 @@ const AboutPage = () => {
                 >
                   {roleLoading ? "Loading..." : "Home"}
                 </button>
-                <button className="bg-gray-700 px-4 py-2 rounded hover:bg-gray-600">
-                  About Page
-                </button>
+                
                 <button className="bg-gray-700 px-4 py-2 rounded hover:bg-gray-600">
                   Catalog
                 </button>
                 <button className="bg-gray-700 px-4 py-2 rounded hover:bg-gray-600">
                   Points
                 </button>
-                //this will need to be handled based on user
-                <button className="bg-gray-700 px-4 py-2 rounded hover:bg-gray-600">
-                  Application
-                </button>
+                
                 <button className="bg-gray-700 px-4 py-2 rounded hover:bg-gray-600">
                   More
                 </button>
