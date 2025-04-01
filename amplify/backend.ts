847574f8954a968622ca5024e3256966fe3e60af
jkthomp <jkthomp@g.clemson.edu>
2025-04-01 13:55:29 -0400

    Some of the backend stuff for notifications.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,14 +1,29 @@
+
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { funct } from './functions/resource';
+import { CustomNotifications } from './custom/CustomNotifications/resource';
 
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
   funct,
+});
+
+const customNotifications = new CustomNotifications(
+  backend.createStack('CustomNotifications'),
+  'CustomNotifications',
+  { sourceAddress: 'sender@example.com' }
+);
+
+backend.addOutput({
+  custom: {
+    topicArn: customNotifications.topic.topicArn,
+    topicName: customNotifications.topic.topicName,
+  },
 });
